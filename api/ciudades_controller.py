c815448a4c92260a0799b0f8a9c3609affe7ed5d
nanigalli <nani16.galli@gmail.com>
2018-03-18 15:24:10 -0300

    Agregue la logica para poder obtener la info necesaria del Json de ciudades para devolversela al cliente

--- api/ciudades_controller.py
+++ api/ciudades_controller.py
@@ -3,6 +3,37 @@ import requests
 from flask import jsonify
 from model.date_time import DateTime
 
+#La agrego ahora solo para usarla de ejemplo
+LISTA_EJEMPLO = [
+  {
+    "id": 707860,
+    "name": "Hurzuf",
+    "country": "UA",
+    "coord": {
+      "lon": 34.283333,
+      "lat": 44.549999
+    }
+  },
+  {
+    "id": 519188,
+    "name": "Novinki",
+    "country": "RU",
+    "coord": {
+      "lon": 37.666668,
+      "lat": 55.683334
+    }
+  },
+  {
+    "id": 1283378,
+    "name": "Gorkhā",
+    "country": "NP",
+    "coord": {
+      "lon": 84.633331,
+      "lat": 28
+    }
+  }
+]
+
 class CiudadesController:
     """Esta clase se encarga de manejar la informacion de las ciudades"""
 
@@ -11,7 +42,16 @@ class CiudadesController:
 
     def obtenerListaCiudadesPaginado(self, pagina):
         """Devuelve el listado de ciudades de una determinada pagina"""
-        resultado = []
-        #Aca va a ir la logica para obtener las ciudades
-        resultado.append('Ciudades...')
+        resultado = self.obtenerListaCiudades(LISTA_EJEMPLO)
         return resultado
+
+    def obtenerListaCiudades(self, listaOriginal):
+        """Armamos la lista de ciudades a devolver"""
+        lista = []
+        for ciudad in listaOriginal:
+            infociudad = {}
+            infociudad['id'] = ciudad['id']
+            infociudad['name'] = ciudad['name']
+            infociudad['country'] = ciudad['country']
+            lista.append(infociudad)
+        return lista
